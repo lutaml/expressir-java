5c1738a694be5bd27363744d3c2a26e2f580e537
Ronald Tse <ronald.tse@ribose.com>
2020-08-25 22:01:29 +0800

    Initial commit, generated express-grammar v1.0

--- README.md
+++
@@ -1,1 +0,0 @@
-# expressir-java
---
+++ generate-parser.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -eu
+
+# Java parser
+# use with grun
+java -jar ~/.m2/repository/org/antlr/antlr4/4.7.3-SNAPSHOT/antlr4-4.7.3-SNAPSHOT-complete.jar \
+    -listener \
+    -visitor \
+    -Dlanguage=Java \
+    Express.g4
+
+javac -cp .:$HOME/.m2/repository/org/antlr/antlr4/4.7.3-SNAPSHOT/antlr4-4.7.3-SNAPSHOT-complete.jar \
+    *.java
